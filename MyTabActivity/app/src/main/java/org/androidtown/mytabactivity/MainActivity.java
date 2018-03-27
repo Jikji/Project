41f5968eae002c63ecb4fbf6691bee374225c6f4
Jikji <shken35@naver.com>
2018-03-27 19:48:40 +0900

    jikji

---
+++ MyTabActivity/app/src/main/java/org/androidtown/mytabactivity/MainActivity.java
@@ -0,0 +1,101 @@
+package org.androidtown.mytabactivity;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+import org.androidtown.mytabactivity.fragment.FourFragment;
+import org.androidtown.mytabactivity.fragment.OneFragment;
+import org.androidtown.mytabactivity.fragment.ThreeFragment;
+import org.androidtown.mytabactivity.fragment.TwoFragment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    private int[] tabIcons = {
+            R.drawable.icon_apple,
+            R.drawable.icon_facebook,
+            R.drawable.icon_dropbox,
+            R.drawable.icon_googleplus,
+            R.drawable.icon_instagram,
+            R.drawable.icon_linkedin,
+            R.drawable.icon_twitter
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
+
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+        setupTabIcons();
+    }
+
+    private void setupTabIcons() {
+        for (int idx = 0; idx <= 6; idx++) {
+            tabLayout.getTabAt(idx).setIcon(tabIcons[idx]);
+        }
+    }
+
+    public void setupViewPager(ViewPager ViewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new OneFragment(), "ONE");
+        adapter.addFragment(new TwoFragment(), "TWO");
+        adapter.addFragment(new ThreeFragment(), "THREE");
+        adapter.addFragment(new FourFragment(), "FOUR");
+        adapter.addFragment(new FourFragment(), "FIVE");
+        adapter.addFragment(new FourFragment(), "SIX");
+        adapter.addFragment(new FourFragment(), "SEVEN");
+        viewPager.setAdapter(adapter);
+    }
+
+    class ViewPagerAdapter extends FragmentPagerAdapter {
+
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
+    }
+}
---
+++ MyTabActivity/app/src/main/java/org/androidtown/mytabactivity/fragment/SevenFragment.java
@@ -0,0 +1,31 @@
+package org.androidtown.mytabactivity.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.androidtown.mytabactivity.R;
+
+/**
+ * Created by Park on 2018-03-26.
+ */
+
+public class SevenFragment extends Fragment {
+    public SevenFragment() {
+
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_seven, container, false);
+    }
+}
